5faa39fb7c0bf2e21161782c112bc04b635b53ed
marcodalessandro76 <marco.dalessandro@isc.cnr.it>
2019-05-05 17:31:31 +0200

    update script for hpc

--- Calculations/Dataset calculator_hpc.py
+++ Calculations/Dataset calculator_hpc.py
@@ -1,16 +1,47 @@
+# Before calling this script (for istance in the launcher slurm script), perform the operatotions:
+# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# export BIGDFT_MPIRUN='mpirun -np $value$'
+
+
 from BigDFT import Datasets as D, Calculators as C, Inputfiles as I, Logfiles as lf
 from BigDFT.Database import Molecules
 from futile.Utils import write
 import numpy as np
-import os, sys
+import matplotlib.pyplot as plt
+import os, sys, yaml
 sys.path.insert(0,'../')
-import StatPol as SP, workflow_hpc as w
+import StatPol as SP, Routines as R
 
-molecule_set = ['CO','N2','H2O','Mg','Mg2','Ar','HCl','CH4','HF','NaCl','Ne','He']
-xc_set = ['lda'] #['lda','pbe']
-psp_set = ['hgh-k']
+basepath = os.getcwd()
 
-for molecule in molecule_set:
-    for xc in xc_set:
-        for psp in psp_set:
-            w.single_study_workflow(term_verb=True,molecule=molecule,xc=xc,psp=psp)
+nsp_dataset = yaml.load(open('nsp_dataset.yaml')) #load the dataset with the list of gs_study
+#nsp_dataset = yaml.load(open('nsp_results.yaml')) #load the dataset with the results already computed
+
+# For testing and computation purposes we split the dataset into several subset made of
+# 10 molecules each (molecules are sorted in alphabetical order before the splitting). So we have
+# 8 subsets (the last one with 5 molecules)
+
+molecules = nsp_dataset.keys()
+molecules.sort()
+subset_length = 10
+subset = {}
+for ind in range(int(len(molecules)/subset_length)+1):
+    subset[ind] = []
+for ind,mol in enumerate(molecules):
+    subset[int(ind/subset_length)].append(mol)
+
+reduced_study_set = [('lda_pt','hgh_k')]#,('pbe0','hgh_k')]
+#('lda_pw','hgh_k'),('pbe','hgh_k'),('pbe','nlcc_aw'),('pbe','nlcc_ss')]
+
+reload(R)
+# mpi and omp are set from above exporting the asscoiated variables
+code=C.SystemCalculator(skip=True,verbose=False)
+
+for mol in ['CO']# subset[0]:
+    data = nsp_dataset[mol]
+    data['results'] = {}
+    for study in data['study']:
+        if study in reduced_study_set:
+            data['results'][study] = {}
+            data['results'][study] = R.nsp_workflow(term_verb=True,molecule=mol,study=study,code=code,data_folder='data_hpc')
+            print ''
