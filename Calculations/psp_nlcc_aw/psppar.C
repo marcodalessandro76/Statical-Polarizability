9eef724b6debaf9c0d333babaf3a6d90b71a84b1
marcodalessandro76 <marco.dalessandro@isc.cnr.it>
2019-05-05 17:39:13 +0200

    added psp files

---
+++ Calculations/psp_nlcc_aw/psppar.C
@@ -0,0 +1,10 @@
+HGH for carbon (C) in PBE with NLCC
+    6    4  100512 zatom, zion, date (ddmmyy)
+   12 -101130 1 0 2002 0     pspcod, IXC, lmax, lloc, mmax, r2well
+        0.31478662  2     -6.92377020      0.96359690 rloc nloc c1 c2
+   2                                       nsep
+        0.30228391  1      9.57595383      s-projector
+        0.36878252  1     -0.00996437      p-projector
+                           0.00000000
+        0.27439936         0.76008206      rcore, qcore (NLCC width and fraction)
+
